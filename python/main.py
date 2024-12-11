58bf45e99e29151b51ac6142d667b0ab4289732f
zhichen <zhichenzhang2@gmail.com>
2024-12-11 00:15:58 +0000

    delete py

--- python/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from scrape_listings import scrape_property_listings
-
-if __name__ == "__main__":
-    # Base URL for property listings
-    base_url = 'https://www.daft.ie/property-for-sale/ireland?from={}&pageSize=20'
-
-    # Start scraping and storing data
-    scrape_property_listings(base_url)
